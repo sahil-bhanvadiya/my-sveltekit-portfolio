957e06fc72229482b4666afdfd30d5a27a0f6c92
sahil-bhanvadiya <sahil.bhanvadiya.dev@gmail.com>
2023-07-11 22:24:38 +0530

    feat: setup storybook with tailwind

---
+++ src/stories/Button.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import "./button.css";
+
+  /**
+   * Is this the principal call to action on the page?
+   */
+  export let primary = false;
+
+  /**
+   * What background color to use
+   */
+  export let backgroundColor: string | undefined = undefined;
+  /**
+   * How large should the button be?
+   */
+  export let size: "small" | "medium" | "large" = "medium";
+  /**
+   * Button contents
+   */
+  export let label: string = "";
+
+  // Change the tailwind bg classes to see the button background change on save
+  $: mode = primary ? "bg-sky-500" : "bg-fuchsia-600";
+
+  $: style = backgroundColor ? `background-color: ${backgroundColor}` : "";
+</script>
+
+<button
+  type="button"
+  class={["storybook-button", `storybook-button--${size}`, mode].join(" ")}
+  {style}
+  on:click
+>
+  {label}
+</button>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // The path to all files using tailwind classes
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
