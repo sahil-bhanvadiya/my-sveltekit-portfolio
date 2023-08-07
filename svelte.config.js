0fe0d789feabf56099b2831b93691921b7b9ad4d
sahil-bhanvadiya <sahil.bhanvadiya.dev@gmail.com>
2023-08-07 17:32:58 +0530

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
